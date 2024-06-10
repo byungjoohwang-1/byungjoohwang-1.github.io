0d299c31bcb9e26d18cb0ab2d71a1b5cb4ce6b96
byungjoo.hwang-1 <jackhwang0210@gmail.com>
2024-06-10 13:03:13 +0900

    jekyll start

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1717991868',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
